7af1a837b2b9c6a8de48fd122666f2035ff29bf4
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-09-30 15:31:43 -0400

    Tay

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
-const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');  
-  
-const number1 = parseFloat(prompt ('Enter the first number: '));  
-const number2 = parseFloat(prompt ('Enter the second number: '));  
-  
-let result; 
-  
-if (operator == '+') { 
-    result = number1 + number2;  
-}  
-else if (operator == '-') { 
-    result = number1 - number2;  
-}  
-else if (operator == '*') { 
-    result = number1 * number2;  
-}  
-else {  
-    result = number1 / number2; 
-}  
-  
-console.log(" Result is" + result);
+const operator = prompt('Enter an operator ( +, -, * or / ): ');
+
+const number1 = parseFloat(prompt('Enter the 1st number: '));
+const number2 = parseFloat(prompt('Enter the 2nd number: '));
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+console.log("Answer is" + result);
